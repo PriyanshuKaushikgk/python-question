afaa9d036f8fea26f2491750cb78e19ab14749a0
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-12-29 21:12:07 +0530

    Fibonacci series

--- 3.Fibonacci_series.py
+++ 3.Fibonacci_series.py
@@ -1,9 +1,9 @@
 # 15. Write a python program to print the Fibonacci series.
-n = int(input("Enter the number of terms: "))
+# n = int(input("Enter the number of terms: "))
 
-a, b = 0, 1
+# a, b = 0, 1
 
-print("Fibonacci Series:")
-for _ in range(n):
-    print(a, end=" ")
-    a, b = b, a + b
+# print("Fibonacci Series:")
+# for _ in range(n):
+#     print(a, end=" ")
+#     a, b = b, a + b

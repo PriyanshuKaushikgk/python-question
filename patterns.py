b5b257d431f1cf1a0264979f90adc3cac7cf7efa
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2025-01-17 23:57:50 +0530

    Add files via upload

--- patterns.py
+++ patterns.py
@@ -4,10 +4,18 @@
 #         print("*",end=" ")
 #     print()
 
-n = int(input("Enter the number of rows:"))
-k=1
-for i in range(1,n+1):
-    for j in range(1,k+1):
-        print("*",end="")
-    k+=2
-    print()
+# n = int(input("Enter the number of rows:"))
+# k=1
+# for i in range(1,n+1):
+#     for j in range(1,k+1):
+#         print("*",end="")
+#     k+=2
+#     print()
+
+# n = int(input("Enter the number of rows:"))
+# k = 1
+# for i in range(1,n+1):
+#     for j in range(1,k+1):
+#         print("1",end=" ")
+#     k+=2
+#     print()
